ff3cd81c275348b2111f5362bdcbbf2a53a7ab38
Blueprint-Codeworks <lautarofnewells@gmail.com>
2025-01-22 19:05:38 -0300

    update

--- src/app/components/WeatherCard/WeatherCard.tsx
+++ src/app/components/WeatherCard/WeatherCard.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { Cloud, Sun, CloudRain, Wind, Droplets, ThermometerSun } from 'lucide-react';
+import {  Sun, CloudRain, Wind, Droplets } from 'lucide-react';
 
 interface WeatherCardProps {
   location?: string;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import WeatherCard from "./components/WeatherCard/page";
+import WeatherCard from "./components/WeatherCard/WeatherCard";
 export default function Home() {
   return (
     <main className="min-h-screen w-full flex items-center justify-center p-6 bg-gradient-to-br from-blue-50 to-gray-100">
